66d63d27191167964b124ce27c820ad101099745
Ghassan Sleyman <ghassan.sleyman1@gmail.com>
2024-12-09 23:04:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+
+app.get('/',(req,res)=>{
+    res.send('Ghassan Sleyman')
+})
+app.get('./students',(req,res)=>{
+    let.data = [
+        {name:'Ghassan',age:20},
+        {name:'Afif',age:21},
+        {name:'Wael',age:18}];
+    res.send(data)
+})
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
